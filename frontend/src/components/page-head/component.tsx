468a2122bdc7072df066225cdc8b051be9549066
Your Name <you@example.com>
2024-12-08 19:27:38 +0300

    better

--- frontend/src/components/page-head/component.tsx
+++ frontend/src/components/page-head/component.tsx
@@ -4,6 +4,8 @@ import styles from './styles.module.css';
 import { Text } from '@gravity-ui/uikit';
 import { Link } from 'react-router';
 
+import profile_pic_url from '../../mocks/assets/photo_2024-11-06_18-41-31.png';
+
 interface Props {
   children: ReactNode;
 }
@@ -15,6 +17,12 @@ export const PageHead = ({ children }: Props) => {
         <Link to="/" className={styles.headLink}>
           <Text variant="display-1">Курсы 1С</Text>
         </Link>
+        <div className={styles.spacer} />
+        <img
+          src={profile_pic_url}
+          className={styles.profilePic}
+          alt={'Аватарка'}
+        />
       </div>
       <div className={styles.children}>{children}</div>
     </div>
